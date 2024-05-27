7fa56ab9a126e44ca20eadb24b2228fa07599428
denis.troller <denis.troller@sonarsource.com>
2024-05-27 17:26:42 +0200

    Add project files.

---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [HttpPost("new")]
+    public IActionResult PostValue(InputModel model)
+    {
+
+        return Ok();
+    }
+}
+
+
+public class InputModel
+{
+    [JsonProperty(Required = Required.Always)]
+    public string Name { get; set; }
+}
+
